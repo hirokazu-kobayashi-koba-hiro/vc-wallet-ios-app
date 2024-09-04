2abeeab7941ae0ed838300818c8edf0a8c098cd9
hirokazu.kobayashi <hirokazu.kobayashi.koba.hiro@gmail.com>
2024-09-05 08:35:06 +0900

    create verifiable credential client

---
+++ vc-wallet-ios-library/vc-wallet-ios-library/VerifiableCredentialsClient.swift
@@ -0,0 +1,14 @@
+//
+//  VerifiableCredentialsClient.swift
+//  vc-wallet-ios-library
+//
+//  Created by 小林弘和 on 2024/09/05.
+//
+
+import Foundation
+
+public class VerifiableCredentialsClient {
+    
+    public static let shared = VerifiableCredentialsClient()
+    
+}
